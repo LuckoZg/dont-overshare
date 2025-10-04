b50b13f8ff99ba58d1828fa19199b7cdfb6e3fa1
lucian <luciantutunovic@gmail.com>
2025-10-04 23:36:38 +0200

    Build version

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/dont-overshare/', // Important: your repo name
   server: {
     headers: {
       'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'none';"
